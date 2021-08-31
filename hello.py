1adda592078ba0f5c7aa0b2a3f218b50642f7d08
ceit2021meckm <89176679+ceit2021meckm@users.noreply.github.com>
2021-08-31 15:22:13 +1000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print("Hello Worlds. Thankyou boys we are good now.I Try This, hey you. Now its oaky. Still Try New Ideas. I can make change"
+print("Hello Worlds. Thankyou boys we are good now.I Try This, hey you. Now its oaky. Still Try New Ideas. I can make change")
