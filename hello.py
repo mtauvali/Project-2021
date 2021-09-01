0aeb3daf71dea77b25b83d0b6fd6920adf741c88
mtauvali <87688078+mtauvali@users.noreply.github.com>
2021-09-02 08:57:28 +1000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print("Hello Worlds. Thankyou boys we are good now.I Try This, hey you. Now its oaky. Still Try New Ideas. I can make change. Boys our project is okay")
+print("Hello Worlds. Thankyou boys we are good now.I Try This, hey you. Now its oaky. Still Try New Ideas. I can make change. Boys our project is okay. Hello Boys")
