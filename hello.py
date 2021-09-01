19a6012775b94798f23af3f5d53fc3e03f80417a
Manu5858 <89439212+Manu5858@users.noreply.github.com>
2021-09-02 09:04:18 +1000

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,1 +1,1 @@
-print("Hello Worlds. Thankyou boys we are good now.I Try This, hey you. Now its oaky. Still Try New Ideas. I can make change. Boys our project is okay. Hello Boys")
+print("Hello Worlds. aThankyou boys we are good now.I Try This, hey you. Now its oaky. Still Try New Ideas. I can make change. Boys our project is okay. Hello Boys")
